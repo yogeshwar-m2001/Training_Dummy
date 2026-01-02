f0ea338277ae5a1c3e2271fd8502b2f587438b91
yogeshwar-m2001 <yogeshwar.m2001@gmail.com>
2026-01-03 00:39:10 +0530

    Initial commit

---
+++ src/test/java/Tests/Test1.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test1() {
+		System.out.println("Test 1 Running");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test 2 Running");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("Test 3 Running");
+	}
+
+	@Test
+	public void test4() {
+		System.out.println("Test 4 Running");
+	}
+
+	@Test
+	public void test5() {
+		System.out.println("Test 5 Running");
+	}
+}
